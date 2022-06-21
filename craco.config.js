0059cd0bf559ee0b8fe66632977082a4fe087714
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-20 23:29:58 -0300

    update dependencies and routes

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,35 @@
-/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.paths.json');
+
 module.exports = {
+  webpack: {
+    alias: {
+      '@api': path.resolve(__dirname, 'src/api/index.ts'),
+      '@components': path.resolve(__dirname, 'src/components/index.ts'),
+      '@icons': path.resolve(__dirname, 'src/icons/index.ts'),
+      '@interfaces': path.resolve(__dirname, 'src/interfaces/index.ts'),
+      '@lang': path.resolve(__dirname, 'src/lang/index.ts'),
+      '@libs': path.resolve(__dirname, 'src/libs/index.ts'),
+      '@routes': path.resolve(__dirname, 'src/routes/index.ts'),
+      '@services': path.resolve(__dirname, 'src/services/index.ts'),
+      '@settings': path.resolve(__dirname, 'src/settings/index.ts'),
+    },
+  },
+  jest: {
+    configure: {
+      preset: 'ts-jest',
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>/',
+      }),
+    },
+  },
   style: {
-    postcss: {
+    postcssOptions: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
