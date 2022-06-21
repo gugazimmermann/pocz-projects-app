128ac31b27c2edb79d78236bdc6f31b54c2305df
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-21 00:06:52 -0300

    remove unnused var

--- src/components/attachments/attachment-show/AttachmentShow.spec.tsx
+++ src/components/attachments/attachment-show/AttachmentShow.spec.tsx
@@ -2,17 +2,10 @@ import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import AttachmentShow from './AttachmentShow';
 
-interface AttachmentInterface {
-  id: string;
-  date: string;
-  name: string;
-  link: string;
-}
-
 describe('AttachmentShow', () => {
   it('should render successfully', () => {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    const selectAttchment = (attachment: AttachmentInterface) => {};
+    const selectAttchment = () => {};
     const att = {
       id: 'AAA',
       date: '01/01/2021 00:00',
