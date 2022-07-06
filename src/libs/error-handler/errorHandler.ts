029f48a30e5029f28cc0627282b788268466e832
Guga Zimmermann <gugazimmermann@gmail.com>
2022-07-06 14:47:16 -0300

    fix principalId error

--- src/libs/error-handler/errorHandler.ts
+++ src/libs/error-handler/errorHandler.ts
@@ -3,7 +3,8 @@ import { Lang } from '@lang';
 
 export function errorHandler(err: unknown): Error {
   if (axios.isAxiosError(err)) {
-    const msg = !err.response ? err : err.response?.data.message;
+    let msg = !err.response ? err : err.response?.data.message;
+    if (msg === 'No principalId set on the Response') msg = Lang.Errors.ServerError;
     throw new Error(msg);
   }
   throw new Error(Lang.Errors.ServerError);
